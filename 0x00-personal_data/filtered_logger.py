97a9f269f30598036b413965da4f9b86974a1c40
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-02 03:50:54 +0100

    Filetered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
+    """ function that obfuscate items"""
     for item in fields:
         message = re.sub(fr'{re.escape(item)}=.*?(?={re.escape(separator)}|$)',
                          f'{item}={redaction}', message)
