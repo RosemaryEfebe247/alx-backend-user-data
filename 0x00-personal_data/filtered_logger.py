15953b5f6844496c736671a8ed0fda4b92837b67
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-02 03:48:35 +0100

    Filtered loger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    """ The function uses re.sub to replace items"""
-    for field in fields:
-        pattern = re.escape(field)
-        return (re.sub(fr'{pattern}=.*?(?={re.escape(separator)}|$)',
-                       f'{field}={redaction}', message))
+    for item in fields:
+        message = re.sub(fr'{re.escape(item)}=.*?(?={re.escape(separator)}|$)',
+                         f'{item}={redaction}', message)
+    return message
