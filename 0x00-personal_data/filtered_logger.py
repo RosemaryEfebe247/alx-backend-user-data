f8907bf87eff81c3d562dcb509ff8b8aab6ae276
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-02 03:12:20 +0100

    filtered_logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A function called filter_datum that returns the log message obfuscated """
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ The function uses re.sub to replace items"""
+    for field in fields:
+        pattern = re.escape(field)
+        message = re.sub(fr'{pattern}=.*?(?={re.escape(separator)}|$)',
+                         f'{field}={redaction}', message)
+    return(message)
