b161bdd4637622cfc218a7475be2102c34aed3ab
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-09 04:50:39 +0100

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" A class that perfoms basic authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ A basic authentication class
+    """
+    pass
