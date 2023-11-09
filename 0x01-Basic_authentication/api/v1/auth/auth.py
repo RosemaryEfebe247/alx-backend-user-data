1e2aeabe379a5798ce52edc59589276626c8a3ba
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-09 04:20:47 +0100

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -24,7 +24,12 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """ Return None
         """
-        return None
+        if request is None:
+            return None
+        authorization_header = request.headers.get('Authorization')
+        if authorization_header is None:
+            return None
+        return authorization_header
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Return None
