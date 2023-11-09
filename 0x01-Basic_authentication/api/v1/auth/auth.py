df626f26ade761ec1fde2bc0badaae8734c3c7bd
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-09 03:17:43 +0100

    define routes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,13 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Return False
         """
-        return False
+        if path is None or excluded_paths is None:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Return None
