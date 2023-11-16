75163670f6936c18871365944321de3bee018973
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-16 01:29:05 +0100

    app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -6,7 +6,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route("/", methods=("GET"), strict_slashes=False)
+@app.route("/", methods=["GET"], strict_slashes=False)
 def welcome() -> str:
     """ Return a payload
     """
