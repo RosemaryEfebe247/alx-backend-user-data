036e89b1f1a9fc05489b407f6478e852a65b135a
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-16 02:33:03 +0100

    resgister user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,17 +1,34 @@
 #!/usr/bin/env python3
 """ The flask application for the app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=("GET",), strict_slashes=False)
+@app.route("/", methods=["GET"], strict_slashes=False)
 def welcome() -> str:
     """ Return a payload
     """
     message = {"message": "Bienvenue"}
     return jsonify(message)
+
+
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """ The endpoint for User registration
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        register = AUTH.register_user(email, password)
+        message = {"email": email, "message": "user created"}
+        return jsonify(message)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
