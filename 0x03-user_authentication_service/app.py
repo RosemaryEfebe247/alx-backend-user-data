fe48b356126b1a3937c95a32e3c40757d6fc3dc0
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-16 01:23:51 +0100

    app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" The flask application for the app
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=("GET"), strict_slashes=False)
+def welcome() -> str:
+    """ Return a payload
+    """
+    message = {"message": "Bienvenue"}
+    return jsonify(message)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
