005f0f65f0c16b0772fd4c0d39c0f5b66b442b41
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-16 02:44:06 +0100

    register

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,8 +26,7 @@ def users() -> str:
     password = request.form.get("password")
     try:
         register = AUTH.register_user(email, password)
-        message = {"email": email, "message": "user created"}
-        return jsonify(message)
+        return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
