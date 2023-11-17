9f067f30e491c9af41afa301ecd2583e7e377f7f
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-18 00:30:35 +0100

    find_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -45,16 +45,32 @@ class DB:
             new_user = None
         return new_user
 
-    def find_user_by(self, **kwargs: Dict) -> User:
-        """ Find users using the keyword args
+    def find_user_by(self, **kwargs) -> User:
+        """Finds a user based on a set of filters.
         """
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
+
+    """ def find_user_by(self, **kwargs: Dict) -> User:
+        Find users using the keyword args
         for key in kwargs.keys():
             if not hasattr(User, key):
                 raise InvalidRequestError
         user = self._session.query(User).filter_by(**kwargs).first()
         if user is None:
             raise NoResultFound
-        return user
+        return user"""
 
     def update_user(self, user_id: int, **kwargs: Dict) -> None:
         """ A method that updates a user
