bdc88e81ac05db160bf272823223015e90e48d17
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-17 05:05:05 +0100

    User credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -12,8 +12,8 @@ def _hash_password(password: str) -> bytes:
     """
     bytes_format = password.encode('utf-8')
     salt = bcrypt.gensalt()
-    hash_pswd = bcrypt.hashpw(bytes_format, salt)
-    return hash_pswd
+    hashed_password = bcrypt.hashpw(bytes_format, salt)
+    return hashed_password
 
 
 class Auth:
@@ -31,5 +31,19 @@ class Auth:
             if user_found is not None:
                 raise ValueError("User {} already exists".format(email))
         except NoResultFound:
-            hashed_pswd = _hash_password(password)
-            self._db.add_user(email, hashed_pswd)
+            hashed_password = _hash_password(password)
+            self._db.add_user(email, hashed_password)
+
+    def valid_login(self, email: str, password: str, *args) -> bool:
+        """ Return True if email exists and matches the password
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                hashed_pswd = user.hashed_password
+                if bcrypt.checkpw(password.encode('utf-8'), hashed_pswd):
+                    return True
+                return False
+            return False
+        except Exception:
+            return False
