5d72b972ecfd686cd4e5256f890138aff5205852
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-11-18 02:10:40 +0100

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ The flask application for the app
 """
-from flask import Flask, jsonify, request
+from flask import abort, Flask, jsonify, request
 
 from auth import Auth
 
@@ -35,7 +35,17 @@ def users() -> str:
 
 
 @app.route("/sessions", methods=["POST"], strict_slashes=False)
-def login() ->
+def login() -> str:
+    """ The login endpoint. Returns Session payload
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email, password) is False:
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
